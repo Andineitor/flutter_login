a797f72e9a3c11a150ecd9d9070859a41fdbbe3d
Andy Loor <andiloor2809@gmail.com>
2023-07-09 20:36:50 -0500

    scrolling implementado

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,16 @@
+import 'package:firebase_multifactor_auth/pagination_without_getx/pagination_without_getx.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
+   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: Text('hola'),
-      ),
+      home: PaginationWithoutGetX(),
     );
   }
 }
+
